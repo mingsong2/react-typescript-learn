9c5698d9d97ee88e8e39e212ecb64f6c1525e934
frank <13732629211@163.com>
2021-07-03 11:09:40 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  const name: string = "jack";
+
+  return (
+    <div className="App">
+      <h1>{name}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
